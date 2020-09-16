930c34b582d3f00474f2c035c8e37b4da3344af3
MuhammadRidwan4404 <71362912+MuhammadRidwan4404@users.noreply.github.com>
2020-09-16 11:37:04 +0700

    Delete Latihan1.java

--- Latihan1.java
+++
@@ -1,8 +0,0 @@
-public class Latihan1{
-    public static void main (String[] args){
-        System.out.println ("Nama : Muhammad Ridwan");
-        System.out.println ("Nim : 6706200023");
-        System.out.println ("Kelas : D3IF 44-04");
-
-    }
-}
