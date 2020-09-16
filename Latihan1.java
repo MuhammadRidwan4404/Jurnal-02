392d2d15fa2ef01844fd5b5d2300035a17204644
MuhammadRidwan4404 <muhhridwan@studen.telkomuniversity.ac.id>
2020-09-16 11:05:56 +0700

    Latihan1.java

---
+++ Latihan1.java
@@ -0,0 +1,8 @@
+public class Latihan1{
+    public static void main (String[] args){
+        System.out.println ("Nama : Muhammad Ridwan");
+        System.out.println ("Nim : 6706200023");
+        System.out.println ("Kelas : D3IF 44-04");
+
+    }
+}
